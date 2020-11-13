ee50c80c09918ceb981e8abacae90978e2f7ed49
jeddou mhammed <jeddou.ehtp@gmail.com>
2020-11-13 18:55:12 +0100

    Create latest comment customTag

--- blog/templatetags/post_tag.py
+++ blog/templatetags/post_tag.py
@@ -1,6 +1,6 @@
 from django import template
 
-from blog.models import Post
+from blog.models import Post, Comment
 
 register = template.Library()
 @register.inclusion_tag('blog/latest_posts.html')  #decorator
@@ -9,6 +9,17 @@ def latest_posts() : #simple python function
     context ={
 
         'l_posts' :Post.objects.all()[0:5]
+
+    }
+    return context
+
+
+@register.inclusion_tag('blog/latest_comments.html')  #decorator
+def latest_comments() : #simple python function
+
+    context ={
+
+        'l_comments' : Comment.objects.filter(active = True)[:5]
 
     }
     return context
