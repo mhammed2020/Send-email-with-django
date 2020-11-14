b76dbe84986392402f97236bb7df381303a5912f
jeddou mhammed <jeddou.ehtp@gmail.com>
2020-11-14 18:07:26 +0100

    Fix update and delete problem in detail post ..

--- blog/templatetags/post_tag.py
+++ blog/templatetags/post_tag.py
@@ -1,6 +1,8 @@
 from django import template
 
 from blog.models import Post, Comment
+from django.contrib.auth.models import User
+
 
 register = template.Library()
 @register.inclusion_tag('blog/latest_posts.html')  #decorator
--- blog/views.py
+++ blog/views.py
@@ -55,6 +55,7 @@ def post_detail(request,post_id) :
     
     post = get_object_or_404(Post,pk=post_id)
     comments = post.comments.all()
+    # contributors = User.objects.all()
    
 
    
